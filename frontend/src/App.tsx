9486eacb630a7baaef58c7d2526d07665a9d2170
bhaveshittadwar <bitsadwar@gmail.com>
2025-05-17 02:29:20 -0400

    feat: setup socket.io client and connect from frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import socket from './socket'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected: ', socket.id)
+    })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    socket.on('test-event', (data) => {
+      console.log('Received test-event: ', data)
+    })
+
+    return () => {
+      socket.off('connect')
+      socket.off('test-event')
+    }
+  }, [])
+
+  return <div>Frontend Connected to Socket.io</div>
 }
 
 export default App
---
+++ frontend/src/socket.ts
@@ -0,0 +1,8 @@
+import { io } from 'socket.io-client'
+
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL ?? 'http://localhost:5001'
+
+export default io(SOCKET_URL, {
+  withCredentials: true // needed since i am using express session - to send session cookies in handshakes
+})
