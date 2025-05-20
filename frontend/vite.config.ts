2762d10fd9ba8e3d9ade3f6f00d3a898849faefb
bhaveshittadwar <bitsadwar@gmail.com>
2025-05-20 14:49:40 -0400

    fix: update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
-// frontend/vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_ALLOWED_HOST],
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5001',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
       }
     }
   }
